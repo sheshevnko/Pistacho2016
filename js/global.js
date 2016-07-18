140c2791ccd0fb203a4b5a0a6c5d2acd01ddc9b4
SergioMendivil_ <sergiojosuemendivilperez@MBP-de-Sergio.lan>
2016-07-17 20:36:26 -0600

    Navegación Lista

--- js/global.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-	$('a').click(function(e){
-		e.preventDefault();
-		$('html, body').stop().animate({scrollTop: $($(this).attr('href')).offset().top}, 1000);
-	});
-});
